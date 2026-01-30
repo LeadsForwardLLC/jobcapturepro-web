5579298c58338930d59f5563047445cb98d7a8b1
Trevor Eddy <trevor@leadsforward.com>
2026-01-30 23:12:17 +0100

    mobile optimization

--- templates/global/header.php
+++ templates/global/header.php
@@ -9,6 +9,7 @@ $pages = jcp_core_get_page_detection();
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class( 'jcp-global-nav-active' ); ?>>
